22504dff49ef53f838c59d209fde4cf8bcf51690
Asia Serrano <asiamserrano@gmail.com>
2023-10-04 01:04:29 -0400

    from command line

--- autosavedBeta/Views/PropertiesView.swift
+++ autosavedBeta/Views/PropertiesView.swift
@@ -8,30 +8,22 @@
 import SwiftUI
 
 struct PropertiesView: View {
+    
+    private let sort: [NSSortDescriptor] = .propertiesSortDesc()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        FilteredListView(.property, sort) { (property: Property) in
+            let key: String = property.get(.secondary)!
+            var value: String? {
+                switch property.typeEnum {
+                case .input: return property.get(.value)
+                case .platform: return property.get(.tertiary)
+                case .mode: return nil
+                }
+            }
+            
+            FormView(key, value)
+        }
     }
     
-//    @ViewBuilder
-    //    public var PropertiesView: some View {
-    //        let sort: [NSSortDescriptor] = .propertiesSortDesc()
-    //        FilteredListView(sort) { (property: Property) in
-    //            let key: String = property.get(.secondary)!
-    //            var value: String? {
-    //                switch property.typeEnum {
-    //                case .input: return property.get(.value)
-    //                case .platform: return property.get(.tertiary)
-    //                case .mode: return nil
-    //                }
-    //            }
-    //
-    //            FormView(key, value)
-    //        }
-    //    }
-}
-
-struct PropertiesView_Previews: PreviewProvider {
-    static var previews: some View {
-        PropertiesView()
-    }
 }
